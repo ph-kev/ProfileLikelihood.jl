b659168ee620b9ee9cb693d46e6114ce156caa7e
ph-kev <phan.kevin.school@gmail.com>
2022-08-16 11:12:34 -0700

    Added some documentation for error_functions.jl

--- src/error_functions.jl
+++ src/error_functions.jl
@@ -1,11 +1,44 @@
+"""
+    relative_error(data::AbstractVector{T}, sol::AbstractVector{T}, noise_level::T) where T<:Real
+
+The objective function for when the noise in the data follow relative error. 
+    
+The objective function is
+```math
+\hat{\\theta} = \arg \min_{\theta} \frac{1}{\eta^2} \sum_{i=1}^{n} \left( \frac{y_i - g(t_i,\theta)}{g(t_i,\theta)} \right)^2 + 2 \sum_{i=1}^n \log g(t_i,\theta)
+```
+where ``\\eta`` is the noise level, ``g(t_i,\\theta)`` is the ``i``th point of the predicted solution, and ``y_i`` is the ``i``th data point. 
+"""
 function relative_error(data::AbstractVector{T}, sol::AbstractVector{T}, noise_level::T) where T<:Real
     return (1 / noise_level^2) * sum(((abs.(sol) - data) ./ abs.(sol)) .^ 2) + 2.0 * sum(log.(abs.(sol)))
 end
 
+"""
+    poisson_error(data::AbstractVector{T}, sol::AbstractVector{T}) where T<:Real
+
+The objective function for when the data follow a Poisson distribution. 
+    
+The objective function is
+```math
+\hat{\theta} = \arg \min_{\theta} -\sum_{i=1}^n \log (g(t_i,u,\theta)^{y_i}) + \sum_{i=1}^n g(t_i,u,\theta)
+```
+where ``g(t_i,\\theta)`` is the ``i``th point of the predicted solution and ``y_i`` is the ``i``th data point.
+"""
 function poisson_error(data::AbstractVector{T}, sol::AbstractVector{T}) where T<:Real
     return 2*(sum(abs.(sol)) - sum(data .* log.(abs.(sol))))
 end
 
+"""
+    const_variance_error(data::AbstractVector{T}, sol::AbstractVector{T}, sigma::T) where T<:Real
+
+The objective function for when the data follow a Normal distribution with mean 0 and known variance ``\\sigma^2``. 
+    
+The objective function is
+```math
+\hat{\theta} = \arg \min_{\theta} \sum_{i=1}^n (y_i - g(t_i,u,\theta))^2 / \sigma^2
+```
+where ``g(t_i,\\theta)`` is the ``i``th point of the predicted solution and ``y_i`` is the ``i``th data point.
+"""
 function const_variance_error(data::AbstractVector{T}, sol::AbstractVector{T}, sigma::T) where T<:Real
     return (1 / sigma^2) * sum((sol - data) .^ 2)
 end
