782f83c6c761c94b9a85d8818bbcc24530a97e5c
ph-kev <phan.kevin.school@gmail.com>
2022-08-17 11:48:42 -0700

    Added types to functions in generating_data.jl

--- src/generating_data.jl
+++ src/generating_data.jl
@@ -1,6 +1,5 @@
-function generate_data(index::Integer, seed::Integer, dist::Function, prob, alg, times::AbstractVector{T}; incidence_obs_status = false, kwargs...) where T<:Real
-    if incidence_obs_status == false
-        # Solve ODE
+function generate_data(index::Integer, seed::Integer, dist::Function, prob::SciMLBase.AbstractDEProblem, alg::SciMLBase.AbstractDEAlgorithm, times::AbstractVector{<:Real}; incidence_obs_status::Bool = false, kwargs...)
+    if incidence_obs_status == false # check if we are generating incidence data or not 
         sol = solve(
             prob,
             alg,
@@ -8,13 +7,13 @@ function generate_data(index::Integer, seed::Integer, dist::Function, prob, alg,
             save_idxs=index;
             kwargs...
         )
-        perfect_data = abs.(sol[1,:])
+        perfect_data = sol[1,:]
     else
         perfect_data = generate_incidence_data(index, prob, alg, times; kwargs...)
     end
     # Set seed for reproducibility 
     Random.seed!(seed)
-    # Create probability distributions to generate noisy data
+    # Use probability distribution to generate data 
     noisy_data_generator = [dist(i) for i in perfect_data]
     # Generate noisy data 
     noisy_data = Vector{Float64}()
@@ -24,7 +23,7 @@ function generate_data(index::Integer, seed::Integer, dist::Function, prob, alg,
     return perfect_data, noisy_data
 end
 
-function generate_incidence_data(index::Integer, prob, alg, times::AbstractVector{T}; kwargs...) where T<:Real
+function generate_incidence_data(index::Integer, prob::SciMLBase.AbstractDEProblem, alg::SciMLBase.AbstractDEAlgorithm, times::AbstractVector{T}; kwargs...) where T<:Real
     sol = solve(
         prob,
         alg,
@@ -32,25 +31,25 @@ function generate_incidence_data(index::Integer, prob, alg, times::AbstractVecto
         save_idxs=index;
         kwargs...
     )
-    cumulativeData = sol[1,:]
+    cumulative_data = sol[1,:] # Generate cumulative data 
     perfect_data = Vector{Float64}()
-    for i in eachindex(cumulativeData)
+    for i in eachindex(cumulative_data)
         if i == 1
-            append!(perfect_data, 0)
+            append!(perfect_data, 0) # First data point of incidence data is always 0
         else 
-            append!(perfect_data, sol(times[i]) - sol(times[i-1]))
+            append!(perfect_data, sol(times[i]) - sol(times[i-1])) # Incidence data is Δ(C(t) - C(t-1))
         end
     end
     return perfect_data
 end 
 
-function generate_incidence_data(sol)
+function generate_incidence_data(sol::AbstractVector{<:Real}) 
     incidence_data = Vector{Float64}()
     for i in eachindex(sol)
         if i == 1
-            append!(incidence_data, 0)
+            append!(incidence_data, 0) # First data point of incidence data is always 0
         else 
-            append!(incidence_data, sol[i] - sol[i-1])
+            append!(incidence_data, sol[i] - sol[i-1]) # Incidence data is Δ(C(t) - C(t-1))
         end
     end
     return incidence_data
