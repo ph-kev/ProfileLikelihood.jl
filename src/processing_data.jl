64fb18252fbb8b9dc3cec9a84b04c34aaf91fa84
ph-kev <phan.kevin.school@gmail.com>
2022-08-19 15:28:54 -0700

    Added docstring to functions in processing_data.jl

--- src/processing_data.jl
+++ src/processing_data.jl
@@ -1,13 +1,17 @@
 """
     find_roots(interpolation, theta::AbstractVector{<:Real}, threshold::Real)
-                  
+
+This finds the intercepts of the threshold and ``χ^2`` which corresponds to the confidence 
+intervals. 
+
+`interpolation` is a function which takes in a single input 
 # Arguments
-- `interpolation`: the array to search
-- `theta::AbstractVector{<:Real}`: 
-- `threshold::Real`: 
+- `interpolation`: Function which takes in a single input ``x`` and output ``χ^2(x)``. This should be an interpolation of the points found using `PL`.
+- `theta::AbstractVector{<:Real}`: Values of θ searched using `PL`.
+- `threshold::Real`: `threshold` found using `find_threshold`. If `pl_const` was used in `PL`, then also add `pl_const` to `threshold`.
 
 # Returns
-- `res`: 
+- `res`: Intercepts of the threshold and ``χ^2`` which corresponds to the confidence interval.
 """
 function find_roots(interpolation, theta::AbstractVector{<:Real}, threshold::Real)
     res = find_zeros((x) -> interpolation(x) - threshold, (theta[1], theta[end]))
